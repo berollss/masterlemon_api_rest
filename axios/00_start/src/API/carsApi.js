6a9b4f8829db6e551570439b1c40354685a385eb
Berta <bertalaz@gmail.com>
2020-03-13 20:40:02 +0100

    Changed api methods to axios

--- axios/00_start/src/API/carsApi.js
+++ axios/00_start/src/API/carsApi.js
@@ -1,1 +1,26 @@
-const baseUrl = 'http://localhost:3050';
+import axios from "axios";
+
+const baseUrl = "http://localhost:3050";
+
+const handleError = err => console.log(err);
+
+export const getAllCars = () => {
+  return axios
+    .get(`${baseUrl}/api/cars`)
+    .then(result => result.data)
+    .catch(handleError);
+};
+
+export const getCarById = id => {
+  return axios
+    .get(`${baseUrl}/api/cars/${id}`)
+    .then(result => result.data)
+    .catch(handleError);
+};
+
+export const addCar = car => {
+  axios
+    .post(`${baseUrl}/api/cars/`, car)
+    .then((result) => result.data)
+    .catch(handleError);
+};
