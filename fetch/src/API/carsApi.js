3a8280d4d5bc714430914b98d4f43d703ef9cf17
Berta <bertalaz@gmail.com>
2020-09-05 11:43:38 +0200

    Added auth server in fecth solution

--- fetch/src/API/carsApi.js
+++ fetch/src/API/carsApi.js
@@ -1,39 +1,48 @@
 const baseUrl = "http://localhost:3050";
 
-const handleError = response => {
+export const httpClientService = (() => {
+  let headers;
+  const getHeaders = () => headers;
+  return {
+    setHeaders: (_headers) => (headers = _headers),
+    getAllCars: async () => {
+      const response = await fetch(`${baseUrl}/api/cars`, {
+        headers: getHeaders(),
+      });
+      if (!response.ok) {
+        handleError(response);
+      }
+      return await response.json();
+    },
+    getCarById: async (id) => {
+      const response = await fetch(`${baseUrl}/api/cars/${id}`, {
+        headers: getHeaders(),
+      });
+      if (!response.ok) {
+        handleError(response);
+      }
+      return await response.json();
+    },
+    addCar: async (car) => {
+      const settings = {
+        method: "POST",
+        headers: getHeaders(),
+        body: JSON.stringify(car),
+      };
+      settings.headers.Accept = "application/json";
+      settings.headers["Content-type"] = "application/json";
+
+      const response = await fetch(`${baseUrl}/api/cars`, settings);
+      if (!response.ok) {
+        handleError(response);
+      }
+      return await response.json();
+    },
+  };
+})();
+
+const handleError = (response) => {
   const error = response.json();
   console.log(error);
   throw Error(response.statusText);
-};
-
-export const getAllCars = async () => {
-  const response = await fetch(`${baseUrl}/api/cars`);
-  if (!response.ok) {
-    handleError(response);
-  }
-  return await response.json();
-};
-
-export const getCarById = async id => {
-  const response = await fetch(`${baseUrl}/api/cars/${id}`);
-  if (!response.ok) {
-    handleError(response);
-  }
-  return await response.json();
-};
-
-export const addCar = async car => {
-  const settings = {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-type": "application/json"
-    },
-    body: JSON.stringify(car)
-  };
-  const response = await fetch(`${baseUrl}/api/cars`, settings);
-  if (!response.ok) {
-    handleError(response);
-  }
-  return await response.json();
 };
--- fetch/src/app.js
+++ fetch/src/app.js
@@ -3,38 +3,68 @@ import {
   retrieveCarId,
   populateEditCarForm,
   retrieveCarForm,
-  cleanTable
+  cleanTable,
 } from "./uiHelpers";
 import { getAllCars, getCarById, addCar } from "./API/carsApi";
+import { login } from "./api/login.service";
+import { httpClientService } from "./API/carsApi";
+
+const readCredentials = () => {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  return {
+    username,
+    password,
+  };
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("login").addEventListener("click", (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    const credentials = readCredentials();
+    login(credentials)
+      .then((data) => {
+        console.log(data);
+        const { access_token } = data;
+        const headers = {
+          Authorization: `Bearer ${access_token}`,
+        };
+        httpClientService.setHeaders(headers);
+      })
+      .catch((err) => console.log(err));
+  });
+});
 
 document.addEventListener("DOMContentLoaded", () => {
   const buttonLoadCars = document.getElementById("loadcars");
-  buttonLoadCars.addEventListener("click", event => {
+  buttonLoadCars.addEventListener("click", (event) => {
     event.stopPropagation();
     cleanTable("cars-table");
-    getAllCars().then(result => {
+    httpClientService.getAllCars().then((result) => {
       addCarRows(result, "cars-table");
     });
   });
 
   const buttonLoadCar = document.getElementById("loadcar");
-  buttonLoadCar.addEventListener("click", event => {
+  buttonLoadCar.addEventListener("click", (event) => {
     event.stopPropagation();
     const carId = retrieveCarId();
-    getCarById(carId).then(r => populateEditCarForm(r));
+    httpClientService.getCarById(carId).then((r) => populateEditCarForm(r));
   });
 
   const buttonAddCar = document.getElementById("add");
-  buttonAddCar.addEventListener("click", event => {
+  buttonAddCar.addEventListener("click", (event) => {
     event.stopPropagation();
     event.preventDefault();
     const car = retrieveCarForm();
-    addCar(car)
-      .then(_ => {
+    httpClientService
+      .addCar(car)
+      .then((_) => {
         cleanTable("cars-table");
-        return getAllCars();
+        return httpClientService.getAllCars();
       })
-      .then(result => {
+      .then((result) => {
         addCarRows(result, "cars-table");
       });
   });
