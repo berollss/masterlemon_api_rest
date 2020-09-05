7e19feb1814f6e37af957ad10d48ef8a7568b9a4
Berta <bertalaz@gmail.com>
2020-09-05 11:44:04 +0200

    Added Auth server in fetch solution

--- server/server.js
+++ server/server.js
@@ -1,27 +1,29 @@
-const express = require('express');
- path = require('path'),
- cookieParser = require('cookie-parser'),
- bodyParser = require('body-parser'),
- cors = require('cors');
-
-const users = require('./routes/users');
-const cars = require('./routes/cars');
+const express = require("express"),
+  //cookieParser = require("cookie-parser"),
+  bodyParser = require("body-parser"),
+  cors = require("cors"),
+  expressjwt = require("express-jwt");
+const users = require("./routes/users");
+const cars = require("./routes/cars");
 
 const app = express();
 
 // setup
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+//app.use(cookieParser());
 app.use(cors());
+const jwtCheck = expressjwt({
+  secret: "mysupersecretkey",
+  algorithms: ["HS256"],
+});
 
-app.use('/api/users', users);
-app.use('/api/cars', cars);
+//app.use("/api/users", jwtCheck, users);
+app.use("/api/cars", jwtCheck, cars);
 
+app.set("port", process.env.PORT || 3050);
+app.listen(app.get("port"));
 
-app.set('port', process.env.PORT || 3050);
-app.listen(app.get('port'));
-
-console.log('Listening on port: ' + app.get('port'));
+console.log("Listening on port: " + app.get("port"));
 
 module.exports = app;
